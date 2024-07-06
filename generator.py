700af22a3dec254cc18acd90cddf01dc5a2c5950
beknazar <helloworld0774@gmail.com>
2024-07-06 17:48:58 +0500

    Vazifa bajarildi

---
+++ generator.py
@@ -0,0 +1,22 @@
+# def toq_sonlar_generatori(b):
+#     a = 1
+#     while a < b:
+#         yield i
+#         a += 2
+
+# generator = toq_sonlar_generatori(10)
+
+# for i in generator:
+#     print(i)
+
+
+# def toq_sonlar_iteratori(q):
+#     d = 1
+#     while d < q:
+#         yield d
+#         d +=2
+
+# iterator = toq_sonlar_iteratori(20)
+
+# for i in iterator:
+#     print(i)
